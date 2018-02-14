8fc10c671c0af93cd38d3b295762a8210f08b05a
Frank Gregor <phranck@cocoanaut.com>
2018-02-14 10:58:27 +0100

    adds support for Swift 3
    • updates manifests (moves former ‘Package.swift’ to ‘Package@swift-4.0.swift’, ‘Package.swift’ now is in Swift 3 format)
    • makes changes in ‘FileKit.swift’ to support Swift 3
    • updates Readme

--- Package.swift
+++ Package.swift
@@ -1,28 +1,11 @@
-// swift-tools-version:4.0
+// swift-tools-version:3.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "FileKit",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "FileKit",
-            targets: ["FileKit"]),
-    ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/IBM-Swift/LoggerAPI.git", .upToNextMajor(from: "1.0.0"))
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "FileKit",
-            dependencies: ["LoggerAPI"]),
-        .testTarget(
-            name: "FileKitTests",
-            dependencies: ["FileKit"]),
+        .Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", majorVersion: 1, minor: 7)
     ]
 )
